0e21a7937fac54a61a2e651ab98fc6f2f9b9f84f
Burglory <cnarv@hotmail.com>
2015-12-17 20:53:19 +0100

    Added updater support

--- updater.py
+++ updater.py
@@ -1,3 +1,21 @@
 import src.configs
+import urllib.request, subprocess, zipfile, os
 
-src.configs.Config("default.conf")
+c = src.configs.Config("default.conf")
+
+url = c.configdictionary["software_source_url"]
+ziplink = url + "/archive/master.zip"
+urllib.request.urlretrieve(ziplink, "_PIRC.zip")
+
+#subprocess.call(["unzip","PIRC.zip","-d","..", "-o"])
+zipf = zipfile.ZipFile("_PIRC.zip",'r')
+newzipf = zipfile.ZipFile("PIRC.zip",'w')
+for name in zipf.namelist():
+	data = zipf.read(name)
+	if len(data) > 0:
+		newzipf.writestr(name.replace("PIRC-master/",""), data)
+
+newzipf.extractall()
+zipf.close()
+newzipf.close()
+os.remove("_PIRC.zip")
