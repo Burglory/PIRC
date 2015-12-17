7537e4ac702cf706fc97713c4a167672dbd269a0
Gootjes <f.c.gootjes@student.rug.nl>
2015-12-17 16:41:21 +0100

    Started with song readers

---
+++ songreaders.py
@@ -0,0 +1,32 @@
+import json, urllib2
+
+class AbstractSongReader(object):
+
+    def read(self):
+        raise NotImplementedError("You cannot run an abstract class!")
+
+class Radio4SongReader(AbstractSongReader):
+
+    def __init__(self):
+        self.req = urllib2.Request('http://www.radio4.nl/data/nowplaying.json')
+
+    def read(self):
+        resp = urllib2.urlopen(self.req)
+        content = resp.read()
+        jsondoc = json.loads(content)
+        return jsondoc["song"]["composer"] + " - " + jsondoc["song"]["title"]
+
+class Radio2SongReader(AbstractSongReader):
+
+    def __init__(self):
+        self.req = urllib2.Request('http://radiobox2.omroep.nl/data/radiobox2/nowonair/2.json')
+
+    def read(self):
+        resp = urllib2.urlopen(self.req)
+        content = resp.read()
+        jsondoc = json.loads(content)
+        return jsondoc["results"][0]["songfile"]["artist"] + " - " + jsondoc["results"][0]["songfile"]["title"]
+
+
+if __name__ == "__main__":
+    print(Radio4SongReader().read())
