c847e91be026c2dc531742c0b41f71289edbc15b
Gootjes <f.c.gootjes@student.rug.nl>
2015-12-17 16:57:31 +0100

    Started with song readers

--- src/remotecalibrator.py
+++ src/remotecalibrator.py
@@ -1,10 +1,13 @@
 import subprocess, os, signal
 import buttons
 
-i = 0
-popen = None
-result = ""
-template = """\
+class RemoteCalibrator(object):
+
+    def __init__(self):
+        self.i = 0
+        self.popen = None
+        self.result = ""
+        self.template = """\
 begin
     prog = lircradiosystem
     button = {b}
@@ -13,48 +16,47 @@ begin
 end
 """
 
-def complete():
-    print("Writing to file.")
-    f = open('buttonsconfig.lirc','w')
-    f.write(result)
-    f.close()
+    def complete(self):
+        print("Writing to file.")
+        f = open('buttonsconfig.lirc','w')
+        f.write(self.result)
+        f.close()
 
-def process(line):
-    button = line.split(" ")[1]
-    if i<len(buttons.Buttons):
-        print(buttons.Buttons[i] + " has been assigned to: " + button)
-        restemplate = template.repace("{b}",button)
-        restemplate = restemplate.replace("{c}",buttons.Buttons[i])
-        if "key-vol" in buttons.Buttons[i]:
-            restemplate = restemplate.replace("repeat = 0", "repeat = 1")
-        result = result + restemplate
-        i=i+1
+    def process(line):
+        button = line.split(" ")[1]
+        if i<len(buttons.Buttons):
+            print(buttons.Buttons[i] + " has been assigned to: " + button)
+            restemplate = template.replace("{b}",button)
+            restemplate = restemplate.replace("{c}",buttons.Buttons[i])
+            if "key-vol" in buttons.Buttons[i]:
+                restemplate = restemplate.replace("repeat = 0", "repeat = 1")
+            self.result = self.result + restemplate
+            i=i+1
         
-    if i<len(buttons.Buttons):
-        print("Please assign: "+buttons.Buttons[i])
-    else:
-        print("Assignment of buttons has been completed.")
-        popen = subprocess.Popen(["kill", str(popen.pid)])
-        complete()
+        if i<len(buttons.Buttons):
+            print("Please assign: "+buttons.Buttons[i])
+        else:
+            print("Assignment of buttons has been completed.")
+            self.popen = subprocess.Popen(["kill", str(self.popen.pid)])
+            complete()
 
-def execute(command):
-    try:
-        popen = subprocess.Popen(command, stdout=subprocess.PIPE)
-        print("Please assign: "+buttons.Buttons[i])
-        lines_iterator = iter(popen.stdout.readline, b"")
-        lastline = ""
-        for line in lines_iterator:
-            linestring = line.decode("UTF-8")
-            if lastline!=linestring:
-                process(linestring)
-            lastline = linestring
-            
-    except KeyboardInterrupt:
-        print("Assignment of buttons has been interrupted.")
-        popen = subprocess.Popen(["kill", str(popen.pid)])
-        #popen.kill()
-        #os.kill(popen.pid, signal.SIGTERM)
+    def execute(command):
+        try:
+            self.popen = subprocess.Popen(command, stdout=subprocess.PIPE)
+            print("Please assign: "+buttons.Buttons[i])
+            lines_iterator = iter(self.popen.stdout.readline, b"")
+            lastline = ""
+            for line in lines_iterator:
+                linestring = line.decode("UTF-8")
+                if lastline!=linestring:
+                    process(linestring)
+                lastline = linestring
+                
+        except KeyboardInterrupt:
+            print("Assignment of buttons has been interrupted and aborted.")
+            self.popen = subprocess.Popen(["kill", str(self.popen.pid)])
 
-execute(["irw"])
+if __name__=="__main__":
+    execute(["irw"])
 
 
